092e738a1f4ea1c7892fde95a28d5a0fd17771b9
roland <roland@alimentaris.eu>
2016-06-11 09:52:06 +0200

    fixed the cable

--- distributionBlocktimes.R
+++ distributionBlocktimes.R
@@ -2,6 +2,7 @@ timestampsHomestead = as.matrix(read.csv(file = "blocktimes.csv"))
 blocktimes= data.frame(day=as.Date(as.POSIXct(timestampsHomestead[-1,], origin="1970-01-01")), blocktime=diff(timestampsHomestead))
 blocktimes= setNames(blocktimes, c("day","blocktime"))
 summary(blocktimes$blocktime)
+quantile(blocktimes$blocktime, c(.9,.95,.99))
 numberOfDays= length(unique(blocktimes$day));
 hist(blocktimes$blocktime, breaks=numberOfDays*24, main = "Distribution of block times", xlab = "block time [s]", freq = FALSE)
 plot(ecdf(blocktimes$blocktime),  main = "CDF of blocktimes", xlab = "block time [s]", ylab="cumulative density")
