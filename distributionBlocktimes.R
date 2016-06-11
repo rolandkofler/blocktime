a5ec502e84a1270aaca645dffc6320913e85c6b9
roland <roland@alimentaris.eu>
2016-06-11 09:34:31 +0200

    now with cdf plot

--- distributionBlocktimes.R
+++ distributionBlocktimes.R
@@ -1,5 +1,7 @@
 timestampsHomestead = as.matrix(read.csv(file = "blocktimes.csv"))
 blocktimes= data.frame(day=as.Date(as.POSIXct(timestampsHomestead[-1,], origin="1970-01-01")), blocktime=diff(timestampsHomestead))
-blocktimes <- setNames(blocktimes, c("day","blocktime"))
+blocktimes= setNames(blocktimes, c("day","blocktime"))
 summary(blocktimes$blocktime)
-hist(blocktimes$blocktime)
+numberOfDays= length(unique(blocktimes$day));
+hist(blocktimes$blocktime, breaks=numberOfDays*24, main = "Distribution of block times", xlab = "block time [s]", freq = FALSE)
+plot(ecdf(blocktimes$blocktime),  main = "CDF of blocktimes", xlab = "block time [s]", ylab="cumulative density")
