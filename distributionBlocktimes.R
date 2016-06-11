22eba6919e0ea5de426ed42e0fcfbbdada1b5083
roland <roland@alimentaris.eu>
2016-06-11 11:13:02 +0200

    made the peak algo generic

--- distributionBlocktimes.R
+++ distributionBlocktimes.R
@@ -5,7 +5,9 @@ summary(blocktimes$blocktime)
 quantile(blocktimes$blocktime, c(.9,.95,.99))
 numberOfDays= length(unique(blocktimes$day));
 dh=hist(blocktimes$blocktime, breaks=numberOfDays*24, main = "Distribution of block times", xlab = "block time [s]", freq = FALSE, col="blue")
-
-abline(v=dh$breaks[10], col="grey", lty=2)
-text(x=dh$breaks[10], y=dh$density[10]+.005, paste(dh$breaks[10],"s"))
+maximumCount=max(dh$counts, na.rm = TRUE)
+maxIndex=which(dh$counts==maximumCount)
+abline(v=dh$breaks[maxIndex], col="grey", lty=2)
+text(x=dh$breaks[maxIndex], y=dh$density[maxIndex]+.005, paste(dh$breaks[maxIndex],"s"))
 #plot(ecdf(blocktimes$blocktime),  main = "CDF of blocktimes", xlab = "block time [s]", ylab="cumulative density")
+
