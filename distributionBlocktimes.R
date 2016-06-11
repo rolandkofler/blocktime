8ba80757196f206a97c157193162cb5faa76619f
roland <roland@alimentaris.eu>
2016-06-11 10:42:50 +0200

    found the peak

--- distributionBlocktimes.R
+++ distributionBlocktimes.R
@@ -4,5 +4,8 @@ blocktimes= setNames(blocktimes, c("day","blocktime"))
 summary(blocktimes$blocktime)
 quantile(blocktimes$blocktime, c(.9,.95,.99))
 numberOfDays= length(unique(blocktimes$day));
-hist(blocktimes$blocktime, breaks=numberOfDays*24, main = "Distribution of block times", xlab = "block time [s]", freq = FALSE)
-plot(ecdf(blocktimes$blocktime),  main = "CDF of blocktimes", xlab = "block time [s]", ylab="cumulative density")
+dh=hist(blocktimes$blocktime, breaks=numberOfDays*24, main = "Distribution of block times", xlab = "block time [s]", freq = FALSE, col="blue")
+
+abline(v=dh$breaks[10], col="grey", lty=2)
+text(x=dh$breaks[10], y=dh$density[10]+.005, paste(dh$breaks[10],"s"))
+#plot(ecdf(blocktimes$blocktime),  main = "CDF of blocktimes", xlab = "block time [s]", ylab="cumulative density")
